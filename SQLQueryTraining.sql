a9d930e4f0dfcf470e21f8aadb05cf8af9bfadb2
Kelum Perera <kelum0823@hotmail.com>
2017-10-01 18:04:00 +0530

    Update SQLQueryTraining.sql

--- SQLQueryTraining.sql
+++ SQLQueryTraining.sql
@@ -628,3 +628,19 @@ GROUP BY
 	,	YEAR(s.OrderDate)
 
 ORDER BY YEAR(s.OrderDate), SUM(s.SalesAmount) DESC;
+
+-- Using PIVOT
+
+USE AdventureWorks2014;
+GO
+SELECT VendorID, [250] AS Emp1, [251] AS Emp2, [256] AS Emp3, [257] AS Emp4, [260] AS Emp5
+FROM 
+(SELECT PurchaseOrderID, EmployeeID, VendorID
+FROM Purchasing.PurchaseOrderHeader) p
+PIVOT
+(
+COUNT (PurchaseOrderID)
+FOR EmployeeID IN
+( [250], [251], [256], [257], [260] )
+) AS pvt
+ORDER BY pvt.VendorID;
